3b1359217f3a6afd9e3079936e29172da124fa9b
annaabbott <annaabbottdesign@gmail.com>
2019-03-10 23:15:18 -0600

    title and info for each film

--- js/main.js
+++ js/main.js
@@ -5,27 +5,100 @@ films.sort(function (a,b) {
     return a.episode_id - b.episode_id;
 });
 
+const filmsDiv = document.getElementById('films');
+for (let i = 0; i < films.length; i++) {
+    const newDiv = makeFilmInfo(films[i]);
+    filmsDiv.appendChild(newDiv);
+}
+
+function makeFilmInfo(film) {
+    // Create a <div> to hold info for a single film.
+    const resultDiv = document.createElement('div');
+
+    // Set the id for the <div> to be "film-1", etc.
+    resultDiv.id = "film-" + film.episode_id;
+
+    // Add the "film-info" class to this <div>
+    resultDiv.classList.add("film-info");
+
+    resultDiv.appendChild(makeTitle(film));
+    resultDiv.appendChild(makeInfoSection("Info:"));
+    resultDiv.appendChild(makeInfoList(film));
+    return resultDiv;
+}
+
+function makeTitle(film) {
+    // Header for film title.
+    let h2 = document.createElement('h2');
+    h2.innerText = `Episode ${film.episode_id}: ${film.title}`;
+
+    return h2;
+}
+
+function makeInfoSection(title) {
+    let h3 = document.createElement("h3");
+    h3.innerText = title;
+
+    return h3;
+}
+
+function makeInfoList(film) {
+    let ul = document.createElement('ul');
+
+    ul.appendChild(makeInfoListItem('Director', film.director));
+    ul.appendChild(makeInfoListItem('Producer', film.producer));
+    ul.appendChild(makeInfoListItem('Release Date', film.release_date));
+
+    return ul;
+}
+
+function makeInfoListItem(key, value) {
+    const li = document.createElement('li');
+
+    const strong = document.createElement('strong');
+    strong.innerText = key + ": ";
+    li.appendChild(strong);
+    
+    li.appendChild(document.createTextNode(value));
+
+    return li;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let html = "";
 for (let film of films) {
-    html += `<h2> ${film.title}</h2>`;
+    html += `<div class="film-info">`;
+    html += `  <h2>Episode ${film.episode_id}: ${film.title}</h2>`;
+    html += `  <button id="film-${film.episode_id}">More Info</button>`;
+    html += makeFilmInfo(film);
+    html += `</div>`;
 }
 
-//This is a comment
 document.getElementById("films").innerHTML = html;
 
+function makeFilmInfo(film) {
+    let html = "";
+    html += `<div id="info-${film.episode_id}">`;
+    html += film.opening_crawl;
+    html += `</div>`;
 
-
-
-let newTitle = films[0]
-
-
-document.querySelector("#display-button").onclick = function () {
-document.querySelector("#title").innerHTML = films[0].title;
+    return html;
 }
-
-
-console.log(films[0])
-
-
+*/
 
 
